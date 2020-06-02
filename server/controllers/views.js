ed16ff6555326a73f3dc5beafb4f5458ac831bcf
Dan <danguenet@gmail.com>
2020-06-01 22:46:08 -0500

    view refactor

--- server/controllers/views.js
+++ server/controllers/views.js
@@ -3,23 +3,23 @@ const renderHome = (req, res) => {
 };
 
 const renderSendAttachBankAccount = (req, res) => {
-  res.render("send-attach-bank-account");
+  res.render("send/attach-bank-account");
 };
 
 const renderSendCreateUser = (req, res) => {
-  res.render("send-create-user");
+  res.render("send/create-user");
 };
 
 const renderGetMasterAccount = (req, res) => {
-  res.render("send-get-master-account");
+  res.render("send/get-master-account");
 };
 
 const renderGetMasterFundingSource = (req, res) => {
-  res.render("send-get-master-funding-source");
+  res.render("send/get-master-funding-source");
 };
 
 const renderSendMoveMoney = (req, res) => {
-  res.render("send-move-money");
+  res.render("send/move-money");
 };
 
 const renderCongrats = (req, res) => {
